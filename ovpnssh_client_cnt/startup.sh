d7d15553c24a35c15705c0c445a0e642c8853762
Fedor Chernyshev <k4route@gmail.com>
2024-10-10 09:12:32 +0000

    added info in README

--- ovpnssh_client_cnt/startup.sh
+++ ovpnssh_client_cnt/startup.sh
@@ -4,7 +4,7 @@ if [ ! -f /root/.ssh/id_rsa ]; then
     ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N ''
 fi
 #ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N '' && \
-route add -host <IP> gw 172.17.0.1
+route add -host 176.31.162.173 gw 192.168.3.1
 systemctl enable secure-tunnel.service && \
 systemctl start secure-tunnel.service
 openvpn --config /etc/openvpn/openvpn.conf 
